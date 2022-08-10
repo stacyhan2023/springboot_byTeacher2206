40c5cd113468ffa76f03ec728f78a05a505bea1e
jason <qlfreesky@gmail.com>
2022-08-10 14:07:56 +0800

    day02

--- src/main/java/com/tedu/springboot2206/controller/UserController.java
+++ src/main/java/com/tedu/springboot2206/controller/UserController.java
@@ -9,8 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class UserController {
 
+    /**
+     *
+     * @param request   请求对象，封装着浏览器发送过来的所有内容
+     * @param response  响应对象，封装着我们即将给浏览器回复的内容
+     */
     @RequestMapping("/regUser")
     public void reg(HttpServletRequest request, HttpServletResponse response){
         System.out.println("开始处理注册！！！！！！！！！！！！！！");
+        /*
+            处理注册的大致过程
+            1:获取用户在注册页面上输入的注册信息(通过请求对象获取浏览器提交的表单数据)
+            2:处理注册
+            3:设置响应对象，将处理结果回馈给浏览器
+         */
     }
 }
